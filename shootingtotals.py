b6b0ac3082f8791483f1238e9fbf55a9d5795c4f
James Pothen <69491494+jamespothen@users.noreply.github.com>
2024-08-05 17:04:59 -0400

    starts changing counting file to use months instead of years

--- shootingtotals.py
+++ shootingtotals.py
@@ -1,32 +1,13 @@
-# infilelist = [
-#     "2011_Yellow_Taxi_Trip_Data.csv",
-#     "2012_Yellow_Taxi_Trip_Data.csv",
-#     "2013_Yellow_Taxi_Trip_Data.csv",
-#     "2014_Yellow_Taxi_Trip_Data.csv",
-#     "2015_Yellow_Taxi_Trip_Data.csv",
-#     "2016_Yellow_Taxi_Trip_Data.csv",
-#     "2017_Yellow_Taxi_Trip_Data.csv",
-#     "2018_Yellow_Taxi_Trip_Data.csv",
-#     "2019_Yellow_Taxi_Trip_Data.csv",
-#     "2020_Yellow_Taxi_Trip_Data.csv",
-#     "2021_Yellow_Taxi_Trip_Data.csv",
-# ]
+# infile = "csv/ShootingsByMonth.csv"
+# outfile = open("shootingmonthlytotals.csv", "w")
 #
-# outfile = open("taxiridetotals.csv", "w")
-#
-# yearcountdict = {
-#     "2011": 0,
-#     "2011": 0,
-#     "2012": 0,
-#     "2013": 0,
-#     "2014": 0,
-#     "2015": 0,
-#     "2016": 0,
-#     "2017": 0,
-#     "2018": 0,
-#     "2019": 0,
-#     "2020": 0,
-#     "2021": 0,
+# monthcountdict = {
+#     "01": 0,
+#     "02": 0,
+#     "03": 0,
+#     "04": 0,
+#     "05": 0,
+#     "06": 0,
 # }
 #
 # for filename in infilelist:
