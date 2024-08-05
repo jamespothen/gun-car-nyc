bd06faac7299875ca2692cb65158da822d03c270
James Pothen <69491494+jamespothen@users.noreply.github.com>
2024-08-05 16:57:18 -0400

    scans sheet and creates sheet with just monthly shootings

--- gundata.py
+++ gundata.py
@@ -1,26 +1,24 @@
 infile = open("csv/NYPD_Shooting_Incident_Data__Year_To_Date__20240727.csv", "r")
-# outfile = open(filedict[file], "w")
+outfile = open("csv/ShootingsByMonth.csv", "w")
 
 
 print(infile)
-# print(filedict[file])
+print(outfile)
 print()
 
 
 
-# for line in infile:
-#     try:
-#         pickuplongitude = line.split(",")[5]
-#         pickuplatitude = line.split(",")[6]
-#         if pickuplongitude != "0" and pickuplatitude != "0":
-#             print(infile)
-#             print(f"{pickuplongitude}, {pickuplatitude}")
-#             print(f"{pickuplongitude}, {pickuplatitude}", file=outfile)
-#             print()
-#     except:
-#         print("Error!")
-#         print("Problem with the following line")
-#         print(line)
+for line in infile:
+    try:
+        murder = line.split(",")[9]
+        month = line.split(",")[1][0:2]
+        if murder == "Y":
+            print(month)
+            print(f"{month}", file=outfile)
+    except:
+        print("Error!")
+        print("Problem with the following line")
+        print(line)
 
 infile.close()
-# outfile.close()
+outfile.close()
