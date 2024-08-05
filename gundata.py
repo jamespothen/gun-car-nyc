03cfbe258c6103d35e49e3323a0fd7221ca7e2c1
James Pothen <69491494+jamespothen@users.noreply.github.com>
2024-08-05 16:42:15 -0400

    adds code to open shooting csv

--- gundata.py
+++ gundata.py
@@ -1,1 +1,26 @@
-#tk
+infile = open("csv/NYPD_Shooting_Incident_Data__Year_To_Date__20240727.csv", "r")
+# outfile = open(filedict[file], "w")
+
+
+print(infile)
+# print(filedict[file])
+print()
+
+
+
+# for line in infile:
+#     try:
+#         pickuplongitude = line.split(",")[5]
+#         pickuplatitude = line.split(",")[6]
+#         if pickuplongitude != "0" and pickuplatitude != "0":
+#             print(infile)
+#             print(f"{pickuplongitude}, {pickuplatitude}")
+#             print(f"{pickuplongitude}, {pickuplatitude}", file=outfile)
+#             print()
+#     except:
+#         print("Error!")
+#         print("Problem with the following line")
+#         print(line)
+
+infile.close()
+# outfile.close()
