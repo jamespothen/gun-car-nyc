f90ee14940fb17542d17f7a0ccbfb42733e18dae
James Pothen <69491494+jamespothen@users.noreply.github.com>
2024-08-06 14:44:47 -0400

    counts total number of people killed by cars per month

---
+++ collisiontotals.py
@@ -0,0 +1,25 @@
+infile = open("csv/CollisionsByMonth.csv")
+# outfile = open("csv/collisionmonthlytotals.csv", "w")
+
+monthcountdict = {
+    "01": 0,
+    "02": 0,
+    "03": 0,
+    "04": 0,
+    "05": 0,
+    "06": 0,
+    "07": 0,
+}
+
+for line in infile:
+    month = line.split(",")[0][0:2]
+    numpeoplekilled = int(line.split(",")[1])
+    monthcountdict[month] += numpeoplekilled
+infile.close()
+
+print(monthcountdict)
+
+# for month in monthcountdict.keys():
+#     print(f"{month}, {monthcountdict[month]}", file=outfile)
+
+# outfile.close()
