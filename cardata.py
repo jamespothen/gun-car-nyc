53b993408762b3908a5cbc50a639c36d65b03854
James Pothen <69491494+jamespothen@users.noreply.github.com>
2024-08-06 14:20:15 -0400

    prints months in 2024 when collisions occurred

--- cardata.py
+++ cardata.py
@@ -6,17 +6,23 @@ print(infile)
 print(outfile)
 print()
 
-# for line in infile:
-#     try:
+for line in infile:
+    try:
+        date = line.split(",")[0]
+        year = date[-4:]
+        # print(year)
+        if year == "2024":
+            month = date[0:2]
+            print(month)
 #         murder = line.split(",")[9]
 #         month = line.split(",")[1][0:2]
 #         if murder == "Y":
 #             print(month)
 #             print(f"{month}", file=outfile)
-#     except:
-#         print("Error!")
-#         print("Problem with the following line")
-#         print(line)
+    except:
+        print("Error!")
+        print("Problem with the following line")
+        print(line)
 
 infile.close()
 outfile.close()
