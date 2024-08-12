9fe80bf7eda244aba40f2557edd40a58c3f405a1
James Pothen <69491494+jamespothen@users.noreply.github.com>
2024-08-12 12:35:24 -0400

    starts printing out murders in subway

--- subwaydata.py
+++ subwaydata.py
@@ -1,22 +1,26 @@
-# infile = open("csv/NYPD_Shooting_Incident_Data__Year_To_Date__20240727.csv", "r")
-# outfile = open("csv/ShootingsByMonth.csv", "w")
-#
-#
-# print(infile)
-# print(outfile)
-# print()
-#
-# for line in infile:
-#     try:
-#         murder = line.split(",")[9]
-#         month = line.split(",")[1][0:2]
-#         if murder == "Y":
-#             print(month)
-#             print(f"{month}", file=outfile)
-#     except:
-#         print("Error!")
-#         print("Problem with the following line")
-#         print(line)
-#
-# infile.close()
-# outfile.close()
+infile = open("csv/MTA_Major_Felonies_20240812.csv", "r")
+outfile = open("csv/SubwayDeathsByMonth.csv", "w")
+
+
+print(infile)
+print(outfile)
+print()
+
+for line in infile:
+    try:
+        crime = line.split(",")[3]
+        year = line.split(",")[0][-4:]
+        if crime == 'Murder' and year == '2024':
+            print(line)
+        # murder = line.split(",")[9]
+        # month = line.split(",")[1][0:2]
+        # if murder == "Y":
+        #     print(month)
+        #     print(f"{month}", file=outfile)
+    except:
+        print("Error!")
+        print("Problem with the following line")
+        print(line)
+
+infile.close()
+outfile.close()
